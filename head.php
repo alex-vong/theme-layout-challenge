4c0f4a58b6d59c2cd516c657b322c663c15aafb5
Alex Vong <avong003@gmail.com>
2021-07-13 13:23:47 -0700

    head

---
+++ head.php
@@ -0,0 +1,25 @@
+
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>SURLTC 2</title>
+	<meta
+		name="description"
+		content="Super Ultraish Responsive Layout and Theming Challenge"
+	/>
+	<meta
+		name="og:description"
+		content="Super Ultraish Responsive Layout and Theming Challenge"
+	/>
+
+	<meta property="og:title" content="Alex Codes a Thing" />
+	<meta property="og:type" content="">
+	<meta property="og:url" content="">
+
+	<meta property="og:image" content="https://peprojects.dev/alpha-1/alexvong/images/rlc.png" />
+	<link rel="shortcut icon" type="image/jpg" href="[[favicon]]" />
+
+	<link rel="stylesheet" href="styles/styles.css" />
+
+</head>
+
