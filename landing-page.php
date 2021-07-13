cbeff7960578d1ed7d6aaca40804aa5d20f2aeb2
Alex Vong <avong003@gmail.com>
2021-07-13 13:24:04 -0700

    landing page

---
+++ landing-page.php
@@ -0,0 +1,11 @@
+<section class="landing-content">
+	<picture>
+		<img src="https://peprojects.dev/images/square.jpg" alt="">
+	</picture>
+	<div class="landing-text">
+		<h1 class="loud-voice">Hello! This is a 'diptych'</h1>
+		<p class="calm-voice">
+			You can call it whatever you want. It's a module that has an image and some supporting text. On a smaller screen it works nicely to stack. On a larger screen it firs side by side.
+		</p>
+	</div>
+</section>
